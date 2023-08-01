087466cb00d3d30b3dfc12c5d27077ce0e9c78e9
zainbinshakoor <yammybajwa09@gmail.com>
2023-08-01 13:42:16 +0500

    feat:pr-card

--- components/Home/FeaturedProducts.tsx
+++ components/Home/FeaturedProducts.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Box, HStack, Text } from "@chakra-ui/react"
 import ProductCard from './ProductCard'
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 const FeaturedProducts = () => {
+    const responsive = {
+        0: { items: 1 },
+        568: { items: 2 },
+        1024: { items: 3 },
+    };
     return (
         <>
             <Box textAlign={"center"}>
@@ -11,12 +18,17 @@ const FeaturedProducts = () => {
                 </Text>
             </Box>
             <HStack justify={"center"}>
-                {[1, 1, 1, 1].map((product: any, index: any) => {
-                    return (
-                        <ProductCard key={index} />
-                    )
-                })}
-            </HStack>
+                <AliceCarousel
+                    responsive={responsive}
+                    controlsStrategy="alternate"
+                >
+                    {[1, 1, 1, 1, 1, 1, 1, 1].map((product: any, index: any) => {
+                        return (
+                                <ProductCard key={index} />
+                        )
+                    })}
+                </AliceCarousel >
+            </HStack >
         </>
     )
 }
--- components/Home/ProductCard.tsx
+++ components/Home/ProductCard.tsx
@@ -12,13 +12,13 @@ const ProductCard = () => {
     }
     return (
         <>
-            <Stack h={360} w={270} m={2} boxShadow={"0px 0px 25px 0px rgba(0, 0, 0, 0.10)"} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <Stack h={360} w={270} m={2} boxShadow={"0px 0px 25px 0px rgba(0, 0, 0, 0.10)"} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} cursor={'pointer'}>
                 <Stack h={"100%"} height={"200%"} align={"center"} justify={"flex-end"} bgImage={"/Assets/chair.png"} bgSize={"contain"} bgPos={"center"} bgColor={"#fff9"} bgRepeat={"no-repeat"} pos={"relative"}>
                     {isMouseEnter &&
                         <HaktoButton text={"View Details"} type='success' />
                     }
                 </Stack>
-                <Box h="100%" textAlign={"center"} bgColor={isMouseEnter ? "secondaryBlue.100" : "transparent"}>
+                <Box h="100%" textAlign={"center"} transition={'.3s ease'} bgColor={isMouseEnter ? "secondaryBlue.100" : "transparent"}>
                     <Text fontSize={24} color={isMouseEnter ? "white" : 'primary.100'}>
                         Cantilever chair
                     </Text>
