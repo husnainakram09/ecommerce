f26d132c450cabea7b6f7feaf4dc00961c21ae3f
zainbinshakoor <yammybajwa09@gmail.com>
2023-08-11 11:30:04 +0500

    feat-trending-section-cmp

--- components/Home/TrendingProduct.tsx
+++ components/Home/TrendingProduct.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect, useState } from "react";
-import { Box, HStack, SimpleGrid, Text } from "@chakra-ui/react"
+import { Box, Flex, HStack, SimpleGrid, Text } from "@chakra-ui/react"
 import TrendingProductCard from "./TrendingProductCard";
+import Image from "next/image";
 interface IProps { };
 
 const ShopEx: FC<IProps> = (props) => {
@@ -9,6 +10,11 @@ const ShopEx: FC<IProps> = (props) => {
         '/Assets/trending2.png',
         '/Assets/trending3.png',
         '/Assets/trending4.png',
+    ]
+    const Discount = [
+        '/Assets/discount.png',
+        '/Assets/discount2.png',
+        '/Assets/discount3.png',
     ]
     return (
         <>
@@ -20,10 +26,48 @@ const ShopEx: FC<IProps> = (props) => {
             <HStack justify={"center"} mt={10}>
                 {Images.map((product: any, index: any) => {
                     return (
-                        <TrendingProductCard key={index} image={product}/>
+                        <TrendingProductCard key={index} image={product} />
                     )
                 })}
             </HStack >
+            <Flex display={'flex'} justify={'space-evenly'} gap={5} mt={5}>
+                <Box bgColor={'#FFF6FB'} h='220px' w={'30%'} p={5} borderRadius={10}>
+                    <Text fontSize={"20px"} color={'#151875'}>23% off in all products</Text>
+                    <Text fontSize={"20px"} color={'#FB2E86'} as={'ins'}>Shop Now</Text>
+                    <Box display={'flex'} justifyContent={'end'}>
+                        <Image src={'/Assets/clock.png'} alt="" width={150} height={150} />
+                    </Box>
+
+                </Box>
+                <Box bgColor={'#EEEFFB'} h='220px' w={'30%'} p={5} borderRadius={10}>
+                    <Text fontSize={"20px"} color={'#151875'}>23% off in all products</Text>
+                    <Text fontSize={"20px"} color={'#FB2E86'} as={'ins'}>View Collection</Text>
+                    <Box display={'flex'} justifyContent={'end'}>
+                        <Image src={'/Assets/drawer.png'} alt="" width={200} height={200} />
+                    </Box>
+
+                </Box>
+                <Box h='220px' w={'30%'}  borderRadius={10}>
+                    {
+                        Discount.map((item, index) => {
+                            return (
+                                <div key={index}>
+                                    <Box display={'flex'} mt={1}>
+                                        <Box bgColor={'#F5F6F8'} p={2} borderRadius={9}>
+                                            <Image src={item} alt="" width={50} height={50} />
+                                        </Box>
+                                        <Box ms={3}>
+                                            <Text fontSize={"16px"} color={'#151875'}>Executive Seat chair</Text>
+                                            <Text fontSize={"16px"} color={'#151875'} as={'del'}>50$</Text>
+                                        </Box>
+                                    </Box>
+                                </div>
+                            )
+                        })
+                    }
+
+                </Box>
+            </Flex>
 
         </>
     );
