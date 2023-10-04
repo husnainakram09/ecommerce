b7481099399c1d891c64bf319857fa6d9be87b40
Dimas-Saputra-ME <saputra.dinas@yahoo.co.id>
2023-10-04 19:51:25 +0700

    style: enhance toggle icon position in navbar

--- styles/theme/layout/components/Navbar.tsx
+++ styles/theme/layout/components/Navbar.tsx
@@ -7,6 +7,7 @@ import {
     IconButton,
     Button,
     Stack,
+    HStack,
     Collapse,
     Icon,
     Popover,
@@ -194,18 +195,20 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
                 _hover={{
                     textDecoration: 'none',
                 }}>
-                <Text fontWeight={600} color={useColorModeValue('gray.600', 'gray.200')}>
-                    {label}
-                </Text>
-                {children && (
-                    <Icon
-                        as={ChevronDownIcon}
-                        transition={'all .25s ease-in-out'}
-                        transform={isOpen ? 'rotate(180deg)' : ''}
-                        w={6}
-                        h={6}
-                    />
-                )}
+                <HStack>
+                    <Text fontWeight={600} color={useColorModeValue('gray.600', 'gray.200')}>
+                        {label}
+                    </Text>
+                    {children && (
+                        <Icon
+                            as={ChevronDownIcon}
+                            transition={'all .25s ease-in-out'}
+                            transform={isOpen ? 'rotate(180deg)' : ''}
+                            w={6}
+                            h={6}
+                        />
+                    )}
+                </HStack>
             </Box>
 
             <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
